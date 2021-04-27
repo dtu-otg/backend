2b0b93dd0edeb6ed1ba84f44387e40a904ae6912
specdrake <akhooda.hooda@gmail.com>
2021-04-27 14:23:05 +0530

    update timetable view perms

--- timetable/views.py
+++ timetable/views.py
@@ -10,30 +10,13 @@ from drf_yasg import openapi
 
 # Create your views here.
 
-# def TimeTableView(request):
-#     """
-#     Endpoint for timetable
-#     """
-#     year = request.GET["year"]
-#     batchgrp = request.GET["batchgrp"]
-#     batchnum_s = request.GET["batchnum"]
-#     try:
-#         batchnum = int(batchnum_s)
-#     except e:
-#         response = HttpResponse('')
-#         response.status_code = 69
-#         return response
-#     tt = ttmaker(year, batchgrp, batchnum, True)
-#     return HttpResponse(tt, content_type='application/json')
-
-
-
 year_param = openapi.Parameter('year', openapi.IN_QUERY, description="year for time table e.g. 2k19", type=openapi.TYPE_STRING)
 batchgrp_param = openapi.Parameter('batchgrp', openapi.IN_QUERY, description="batch group time table e.g. A / B", type=openapi.TYPE_STRING)
 batchnum_param = openapi.Parameter('batchnum', openapi.IN_QUERY, description="batch number for time table e.g 1, 2, 3, etc", type=openapi.TYPE_INTEGER)
 
 class TimeTableView(GenericAPIView):
     # permission_classes = [Authenticated]
+
     @swagger_auto_schema(operation_description="get timetable", manual_parameters=[year_param, batchgrp_param, batchnum_param])
     def get(self, request):
         year = request.GET["year"]
